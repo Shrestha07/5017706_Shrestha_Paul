674f3b6708e54783450cd081d79922d90d5858c2
Shrestha Paul <144060878+Shrestha07@users.noreply.github.com>
2024-08-14 02:38:21 +0530

    Add files via upload

---
+++ Week 3_Spring Data JPA and Hibernate/Exercise 4/EmployeeManagementSystem/src/main/java/com/shrestha/employeemanagementsystem/entity/Department.java
@@ -0,0 +1,22 @@
+package com.shrestha.employeemanagementsystem.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@Entity
+@Table(name = "departments")
+public class Department {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @OneToMany(mappedBy = "department", cascade = CascadeType.ALL)
+    private List<Employee> employees;
+}
---
+++ Week 3_Spring Data JPA and Hibernate/Exercise 4/EmployeeManagementSystem/src/main/java/com/shrestha/employeemanagementsystem/entity/Employee.java
@@ -0,0 +1,24 @@
+package com.shrestha.employeemanagementsystem.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "employees")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @ManyToOne
+    @JoinColumn(name = "department_id", nullable = false)
+    private com.shrestha.employeemanagementsystem.entity.Department department;
+}
